897778d2caf26beaedb798c37e16a3c56100d482
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-14 15:19:10 +0300

    +

--- tasks/task.js
+++ tasks/task.js
@@ -1,17 +1,14 @@
-const arr = [1, 2, 3, [1, 23, 4], [1, [2, 3, 4]]]
+const arr = [1, 2, 3, [1, 23, 4], [1, [2, 3, 4]],5];
 
 function flat(n) {
-    let res = []
-    n.forEach(item => {
-        if (Array.isArray(item)) {
-            res = res.concat(
-                flat(item)
-            )
-        } else {
-            res.push(item)
-        }
-    })
-    return res.reduce((a, c) => a + c)
+  let res = [];
+  n.forEach((item) => {
+    if (Array.isArray(item)) {
+      res = res.concat(flat(item));
+    } else {
+      res.push(item);
+    }
+  });
+  return res.reduce((a, c) => a + c);
 }
-console.log(flat(arr))
-
+console.log(flat(arr));
